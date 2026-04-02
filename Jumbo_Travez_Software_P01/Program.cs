829249d106fa69f7945093e490e8debb03097fa4
Fary-T <fary45161@gmail.com>
2026-04-02 09:15:52 -0500

    Jumbo Travez: 9:15 se arreglo el metodo cancelar de acuerdo a clean arquitecture

--- Jumbo_Travez_Software_P01/Program.cs
+++ Jumbo_Travez_Software_P01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Jumbo_Travez_Software_P01;
+﻿using System.Net.NetworkInformation;
+
+namespace Jumbo_Travez_Software_P01;
 
 internal class Program
 {
@@ -8,15 +10,32 @@ internal class Program
         public Guid PatientId { get; private set; }
         public Guid DentistId { get; private set; }
         public AppointmentStatus Status { get; private set; }
-        public Appointment(Guid _patientId, Guid _DentistId)
+        public Appointment(Guid _patientId, Guid _dentistId)
         {
             if (_patientId == Guid.Empty)
             {
                 throw new BussinessRuleException("El paciente es requerido");
             }
-             if () { }
-    }
-            
+             if (_dentistId == Guid.Empty )
+            {
+                throw new BusinessRuleException("El dentista es requerido");
+            }
+            Id = Guid.CreateVersion7();
+            _patientId = PatientId;
+            _dentistId = DentistId;
+            Status = AppointmentStatus.Scheduled;
         }
+        public void Cancel()
+        {
+            if(Status != AppointmentStatus.Scheduled)
+            {
+                throw new BussinessRuleException("No se puede cancelar una cita programda");
+            }
+            
+            Status = Appoitment.Cancelled
+        }
+            
     }
+
+}
 
