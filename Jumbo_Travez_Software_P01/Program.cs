e2394e96b28ebc87b1fab50ab03309153fb5608e
Fary-T <fary45161@gmail.com>
2026-04-02 08:50:12 -0500

    Travez Jumbo: 8:49 se inicio el primer commit

---
+++ Jumbo_Travez_Software_P01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Jumbo_Travez_Software_P01;
+
+internal class Program
+{
+        public class Appointment
+    {
+        public Guid Id { get; private set; }
+        public Guid PatientId { get; private set; }
+        public Guid DentistId { get; private set; }
+        public AppointmentStatus Status { get; private set; }
+        public void Cancel(Appointment appointment)
+        {
+            if (appointment.Status == AppointmentStatus.Completed)
+            {
+                throw new Exception("No se puede cancelar");
+            }
+            appointment.Status = AppointmentStatus.Cancelled;
+        }
+    }
+}
+}
